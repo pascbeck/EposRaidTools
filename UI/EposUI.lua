6f27d7dfb9136422bf14ae548ffedae3fc5cc361
Becker <pascal.becker@sdui.de>
2025-06-04 11:19:33 +0200

    Epos Raid Tools - code quality improvements

--- UI/EposUI.lua
+++ UI/EposUI.lua
@@ -8,8 +8,15 @@ local WA = _G["WeakAuras"]
 local ui_panel_options = {
     UseStatusBar = true
 }
-local EposUI = DF:CreateSimplePanel(UIParent, Epos.Constants.window_width, Epos.Constants.window_height, "|cFF00FFFFEpos|r Raid Tools", "EposUI",
-    ui_panel_options)
+
+local EposUI = DF:CreateSimplePanel(
+    UIParent,
+    Epos.Constants.window_width,
+    Epos.Constants.window_height,
+    "|cFF00FFFFEpos|r Raid Tools",
+    "EposUI",
+    ui_panel_options
+)
 EposUI:SetPoint("CENTER")
 EposUI:SetFrameStrata("HIGH")
 DF:BuildStatusbarAuthorInfo(EposUI.StatusBar, _, "x |cFF00FFFFbird|r")
@@ -20,113 +27,150 @@ function EposUI:Init()
     EposUI:SetScale(EposRT.EposUI.scale)
 
     -- Create the tab container
-    local tabContainer = DF:CreateTabContainer(EposUI, "Epos", "EposUI_Tab", {
+    local tabContainer = DF:CreateTabContainer(
+        EposUI,
+        "Epos",
+        "EposUI_Tab",
         {
-			name 		= "Database",
-			text 		= "Database"
-		},
+            { name = "Database",  text = "Database"  },
+            { name = "Crests",    text = "Crests"    },
+            { name = "WeakAuras", text = "WeakAuras" },
+            { name = "AddOns",    text = "AddOns"    },
+            { name = "Settings",  text = "Settings"  },
+            { name = "Setup",     text = "Setup"     },
+        },
         {
-			name 		= "Crests",
-			text 		= "Crests"
-		},
-        {
-			name 		= "WeakAuras",
-			text 		= "WeakAuras"
-		},
-        {
-			name 		= "AddOns",
-			text 		= "AddOns"
-		},
-        {
-			name 		= "Settings",
-			text 		= "Settings"
-		},
-        {
-			name 		= "Setup",
-			text 		= "Setup"
-		},
-    }, {
-        width = Epos.Constants.window_width,
-        height = Epos.Constants.window_height - 5,
-        backdrop_color = { 0, 0, 0, 0.2 },
-        backdrop_border_color = { 0.1, 0.1, 0.1, 0.4 }
-    })
+            width                = Epos.Constants.window_width,
+            height               = Epos.Constants.window_height - 5,
+            backdrop_color       = { 0, 0, 0, 0.2 },
+            backdrop_border_color = { 0.1, 0.1, 0.1, 0.4 },
+        }
+    )
+    tabContainer:SetPoint("CENTER", EposUI, "CENTER", 0, 0)
 
-	tabContainer:SetPoint("CENTER", EposUI, "CENTER", 0, 0)
-
-	local roster_tab 			= tabContainer:GetTabFrameByName("Database")
-    local crests_tab 			= tabContainer:GetTabFrameByName("Crests")
-    local weakauras_tab 		= tabContainer:GetTabFrameByName("WeakAuras")
-    local addons_tab 			= tabContainer:GetTabFrameByName("AddOns")
-    local settings_tab 			= tabContainer:GetTabFrameByName("Settings")
-    local setup_tab 			= tabContainer:GetTabFrameByName("Setup")
+    local roster_tab    = tabContainer:GetTabFrameByName("Database")
+    local crests_tab    = tabContainer:GetTabFrameByName("Crests")
+    local weakauras_tab = tabContainer:GetTabFrameByName("WeakAuras")
+    local addons_tab    = tabContainer:GetTabFrameByName("AddOns")
+    local settings_tab  = tabContainer:GetTabFrameByName("Settings")
+    local setup_tab     = tabContainer:GetTabFrameByName("Setup")
 
     local settings_options_table = {
-		{ type = "label", get = function() return "General Options" end, text_template = DF:GetTemplate("font", "ORANGE_FONT_TEMPLATE") },
+        {
+            type          = "label",
+            get           = function() return "General Options" end,
+            text_template = DF:GetTemplate("font", "ORANGE_FONT_TEMPLATE"),
+        },
         {
             type = "toggle",
             boxfirst = true,
             name = "Disable Minimap Button",
             desc = "Hide the minimap button.",
             get = function() return EposRT.Settings["Minimap"].hide end,
-            set = function(self, fixedparam, value)
+            set = function(self, fixedParam, value)
                 EposRT.Settings["Minimap"].hide = value
                 LDBIcon:Refresh("EposRT", EposRT.Settings["Minimap"])
             end,
         },
-		{
+        {
             type = "toggle",
             boxfirst = true,
             name = "Enable Debug Mode",
             desc = "Enables Debug Mode, which bypasses certain restrictions like checking for active encounter / combat / being in a raid",
             get = function() return EposRT.Settings["Debug"] end,
-            set = function(self, fixedparam, value)
+            set = function(self, fixedParam, value)
                 EposRT.Settings["Debug"] = value
             end,
         },
-		{ type = "break" },
-		{
+        { type = "break" },
+        {
             type = "select",
             get = function() return "MEDIUM" end,
             values = function() return {} end,
             name = "Frame Strata",
             desc = "Choose Frame Strata for Epos Raid Tools.",
-            nocombat = true
-        }
+            nocombat = true,
+        },
     }
 
-	-- weakauras
-    DF:BuildMenu(weakauras_tab, {}, 10, -100, Epos.Constants.window_height - 10, false, Epos.Constants.templates.text,
-        Epos.Constants.templates.dropdown, Epos.Constants.templates.switch, true, Epos.Constants.templates.slider, Epos.Constants.templates.button,
-        nil)
-	-- addons
-    DF:BuildMenu(addons_tab, {}, 10, -100, Epos.Constants.window_height - 10, false, Epos.Constants.templates.text,
-        Epos.Constants.templates.dropdown, Epos.Constants.templates.switch, true, Epos.Constants.templates.slider, Epos.Constants.templates.button,
-        nil)
-	-- settings
-    DF:BuildMenu(settings_tab, settings_options_table, 10, -100, Epos.Constants.window_height - 10, false, Epos.Constants.templates.text,
-        Epos.Constants.templates.dropdown, Epos.Constants.templates.switch, true, Epos.Constants.templates.slider, Epos.Constants.templates.button,
-        nil)
-	-- setup
-    DF:BuildMenu(setup_tab, {}, 10, -100, Epos.Constants.window_height - 10, false, Epos.Constants.templates.text,
-        Epos.Constants.templates.dropdown, Epos.Constants.templates.switch, true, Epos.Constants.templates.slider, Epos.Constants.templates.button,
-        nil)
+    -- WeakAuras tab (empty menu)
+    DF:BuildMenu(
+        weakauras_tab,
+        {},
+        10, -100,                           -- x, y offset
+        Epos.Constants.window_height - 10,  -- height
+        false,
+        Epos.Constants.templates.text,
+        Epos.Constants.templates.dropdown,
+        Epos.Constants.templates.switch,
+        true,
+        Epos.Constants.templates.slider,
+        Epos.Constants.templates.button,
+        nil
+    )
 
-	-- Build roster UI
-    EposUI.roster_tab  		 = BuildRosterTab(roster_tab)
-    EposUI.database_options  = BuildTrackingOptions()
-	EposUI.blacklist_frame 	 = BuildBlacklistUI()
+    -- AddOns tab (empty menu)
+    DF:BuildMenu(
+        addons_tab,
+        {},
+        10, -100,
+        Epos.Constants.window_height - 10,
+        false,
+        Epos.Constants.templates.text,
+        Epos.Constants.templates.dropdown,
+        Epos.Constants.templates.switch,
+        true,
+        Epos.Constants.templates.slider,
+        Epos.Constants.templates.button,
+        nil
+    )
 
-	-- Build crest UI
-    EposUI.crests_tab  		 = BuildCrestsTab(crests_tab)
-    EposUI.crests_options    = BuildCrestsOptions()
+    -- Settings tab
+    DF:BuildMenu(
+        settings_tab,
+        settings_options_table,
+        10, -100,
+        Epos.Constants.window_height - 10,
+        false,
+        Epos.Constants.templates.text,
+        Epos.Constants.templates.dropdown,
+        Epos.Constants.templates.switch,
+        true,
+        Epos.Constants.templates.slider,
+        Epos.Constants.templates.button,
+        nil
+    )
 
-	-- Version Number in status bar
+    -- Setup tab (empty menu)
+    DF:BuildMenu(
+        setup_tab,
+        {},
+        10, -100,
+        Epos.Constants.window_height - 10,
+        false,
+        Epos.Constants.templates.text,
+        Epos.Constants.templates.dropdown,
+        Epos.Constants.templates.switch,
+        true,
+        Epos.Constants.templates.slider,
+        Epos.Constants.templates.button,
+        nil
+    )
+
+    -- Build roster UI
+    EposUI.roster_tab       = BuildRosterTab(roster_tab)
+    EposUI.database_options = BuildTrackingOptions()
+    EposUI.blacklist_frame  = BuildBlacklistUI()
+
+    -- Build crest UI
+    EposUI.crests_tab       = BuildCrestsTab(crests_tab)
+    EposUI.crests_options   = BuildCrestsOptions()
+
+    -- Version number in status bar
     local versionTitle = C_AddOns.GetAddOnMetadata("EposRaidTools", "Title")
-    local verisonNumber = C_AddOns.GetAddOnMetadata("EposRaidTools", "Version")
-    local statusBarText = versionTitle .. " v" .. verisonNumber
+    local versionNumber = C_AddOns.GetAddOnMetadata("EposRaidTools", "Version")
+    local statusBarText = versionTitle .. " v" .. versionNumber
     EposUI.StatusBar.authorName:SetText(statusBarText)
-
 end
 
 function EposUI:ToggleMainFrame()
--- UI/Roster/Blacklist.lua
+++ UI/Roster/Blacklist.lua
@@ -1,35 +1,50 @@
 local _, Epos = ...
-local DF  = _G["DetailsFramework"]
+local DF = _G["DetailsFramework"]
 
 function BuildBlacklistUI()
     EposRT.Blacklist = EposRT.Blacklist or {}
 
     local blacklist_frame = DF:CreateSimplePanel(
-        UIParent, 485, 420,
-        "Blacklist", "BlacklistEditFrame",
+        UIParent,
+        485,
+        420,
+        "Blacklist",
+        "BlacklistEditFrame",
         { DontRightClickClose = true }
     )
     blacklist_frame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
 
+    --- PrepareData: gather all blacklisted player names
     local function PrepareData()
         local data = {}
         for fullName in pairs(EposRT.Blacklist) do
             tinsert(data, { fullName = fullName })
         end
-        table.sort(data, function(a, b) return a.fullName < b.fullName end)
+
+        table.sort(data, function(a, b)
+            return a.fullName < b.fullName
+        end)
+
         return data
     end
 
+    --- MasterRefresh: clears and repopulates the scrollbox with updated data
     local function MasterRefresh(self)
         local data = PrepareData()
         self:SetData(data)
         self:Refresh()
     end
 
+    --- refresh: populate each scroll line with player name
+    -- @param self       The scrollbox object
+    -- @param data       Table containing blacklist entries
+    -- @param offset     Starting index offset into data table
+    -- @param totalLines Number of line frames to update
     local function refresh(self, data, offset, totalLines)
         for i = 1, totalLines do
             local index = i + offset
             local entry = data[index]
+
             if entry then
                 local line = self:GetLine(i)
                 line.fullName = entry.fullName
@@ -38,16 +53,30 @@ function BuildBlacklistUI()
         end
     end
 
+    --- createLineFunc: create one row in the scrollbox showing player name and delete button
+    -- @param self  Frame  Parent scrollbox frame
+    -- @param index number Line index (used for vertical offset)
+    -- @return Frame A configured line frame with:
+    --   - playerText   (FontString): Player name
+    --   - deleteButton (Button): Remove this blacklist entry
     local function createLineFunc(self, index)
         local line = CreateFrame("Frame", "$parentLine" .. index, self, "BackdropTemplate")
-        line:SetPoint("TOPLEFT", self, "TOPLEFT", 1, -((index - 1) * (self.LineHeight)) - 1)
+        line:SetPoint(
+            "TOPLEFT",
+            self,
+            "TOPLEFT",
+            1,
+            -((index - 1) * (self.LineHeight)) - 1
+        )
         line:SetSize(self:GetWidth() - 2, self.LineHeight)
         DF:ApplyStandardBackdrop(line)
 
+        -- Player name label
         line.playerText = DF:CreateLabel(line, "")
         line.playerText:SetPoint("LEFT", line, "LEFT", 5, 0)
         line.playerText:SetTextColor(1, 1, 1)
 
+        -- Delete button (12x12) at far right
         line.deleteButton = DF:CreateButton(line, function()
             if line.fullName then
                 EposRT.Blacklist[line.fullName] = nil
@@ -57,6 +86,7 @@ function BuildBlacklistUI()
                 end
             end
         end, 12, 12)
+
         line.deleteButton:SetNormalTexture([[Interface\GLUES\LOGIN\Glues-CheckBox-Check]])
         line.deleteButton:SetHighlightTexture([[Interface\GLUES\LOGIN\Glues-CheckBox-Check]])
         line.deleteButton:SetPushedTexture([[Interface\GLUES\LOGIN\Glues-CheckBox-Check]])
@@ -68,7 +98,9 @@ function BuildBlacklistUI()
         return line
     end
 
+    --- ScrollBox Setup
     local scrollLines = 15
+
     local blacklist_scrollbox = DF:CreateScrollBox(
         blacklist_frame,
         "$parentBlacklistScrollBox",
@@ -84,17 +116,23 @@ function BuildBlacklistUI()
     blacklist_scrollbox:SetPoint("TOPLEFT", blacklist_frame, "TOPLEFT", 10, -50)
     blacklist_scrollbox.MasterRefresh = MasterRefresh
     DF:ReskinSlider(blacklist_scrollbox)
+    blacklist_scrollbox.ReajustNumFrames = true
 
+    -- Create exactly as many line frames as will fit
     for i = 1, scrollLines do
         blacklist_scrollbox:CreateLine(createLineFunc)
     end
 
-    DF:CreateLabel(blacklist_frame, "Player Name", 11):SetPoint("TOPLEFT", blacklist_frame, "TOPLEFT", 20, -30)
+    -- Column header label
+    DF:CreateLabel(blacklist_frame, "Player Name", 11)
+        :SetPoint("TOPLEFT", blacklist_frame, "TOPLEFT", 20, -30)
 
+    -- Refresh on show
     blacklist_scrollbox:SetScript("OnShow", function(self)
         self:MasterRefresh()
     end)
 
+    --- Input area for adding new player to blacklist
     local new_label = DF:CreateLabel(blacklist_frame, "New Player:", 11)
     new_label:SetPoint("TOPLEFT", blacklist_scrollbox, "BOTTOMLEFT", 0, -20)
 
@@ -107,18 +145,22 @@ function BuildBlacklistUI()
         if input ~= "" then
             local hasRealm = strfind(input, "%-")
             local fullName = input
+
             if not hasRealm then
                 local realm = GetRealmName():gsub("%s+", "")
                 fullName = input .. "-" .. realm
             end
+
             EposRT.Blacklist[fullName] = true
             new_entry:SetText("")
             blacklist_scrollbox:MasterRefresh()
+
             if EposUI.roster_tab then
                 EposUI.roster_tab:MasterRefresh()
             end
         end
     end, 60, 20, "Add")
+
     add_button:SetPoint("LEFT", new_entry, "RIGHT", 10, 0)
     add_button:SetTemplate(DF:GetTemplate("button", "OPTIONS_BUTTON_TEMPLATE"))
 
